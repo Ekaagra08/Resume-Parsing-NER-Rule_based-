24b085f67767d4f03cd4cb3c1a8189c53a0b9d42
Ekaagra dubey <ekaagra@gmail.com>
2022-01-25 23:47:37 +0530

    updated customo_spacy_model

--- Custom_spacy_model.py
+++ Custom_spacy_model.py
@@ -82,92 +82,35 @@ def trim_entity_spans(data: list) -> list:
   
 train_data_clean = trim_entity_spans(convert_json_for_spacy("/content/drive/MyDrive/Resume Parsing (NER + Rule-based)/traindata.json"))
   
+
+def train_spacyNER():
+
+  # creating blank eng-language class and add the built-in pipeline components to the pipeline
+  c_nlp = spacy.blank("en")
+  if 'ner' not in c_nlp.pipe_names:
+    ner = c_nlp.create_pipe('ner')
+    c_nlp.add_pipe(ner,last = True)
+
+  # adding custom lables from resume
+  for _, annotation in train_data_clean:
+    for ent in annotation.get('entities'):
+      ner.add_label(ent[2])    
+
+  # other pipes to disabled during training
+  other_pipes = [pipe for pipe in c_nlp.pipe_names if pipe != 'ner']
+  with c_nlp.disable_pipes(*other_pipes):  # only train NER
+    optimizer = c_nlp.begin_training()
+    for itn in range(10):
+      print("Statring iteration " + str(itn + 1))
+      random.shuffle(train_data_clean)
+      losses = {}
+      for text, annotations in train_data_clean:
+        c_nlp.update(
+            [text],  # batch of texts
+            [annotations],  # batch of annotations
+            drop=0.2,  # dropout - make it harder to memorise data
+            sgd=optimizer,  # callable to update weights
+            losses=losses)
+      print(losses)
+  return c_nlp
   
- ​@​plac​.​annotations​( 
- ​    ​model​=​(​"Model name. Defaults to blank 'en' model."​, ​"option"​, ​"m"​, ​str​), 
- ​    ​new_model_name​=​(​"New model name for model meta."​, ​"option"​, ​"nm"​, ​str​), 
- ​    ​output_dir​=​(​"Optional output directory"​, ​"option"​, ​"o"​, ​Path​), 
- ​    ​n_iter​=​(​"Number of training iterations"​, ​"option"​, ​"n"​, ​int​), 
- ​) 
- ​def​ ​main​( 
- ​    ​model​=​None​, 
- ​    ​new_model_name​=​"training"​, 
- ​    ​output_dir​=​'/home/omkarpathak27/Downloads/zipped/pyresparser/pyresparser'​, 
- ​    ​n_iter​=​30 
- ​): 
- ​    ​"""Set up the pipeline and entity recognizer, and train the new entity.""" 
- ​    ​random​.​seed​(​0​) 
- ​    ​if​ ​model​ ​is​ ​not​ ​None​: 
- ​        ​nlp​ ​=​ ​spacy​.​load​(​model​)  ​# load existing spaCy model 
- ​        ​print​(​"Loaded model '%s'"​ ​%​ ​model​) 
- ​    ​else​: 
- ​        ​nlp​ ​=​ ​spacy​.​blank​(​"en"​)  ​# create blank Language class 
- ​        ​print​(​"Created blank 'en' model"​) 
- ​    ​# Add entity recognizer to model if it's not in the pipeline 
- ​    ​# nlp.create_pipe works for built-ins that are registered with spaCy 
-  
- ​    ​if​ ​"ner"​ ​not​ ​in​ ​nlp​.​pipe_names​: 
- ​        ​print​(​"Creating new pipe"​) 
- ​        ​ner​ ​=​ ​nlp​.​create_pipe​(​"ner"​) 
- ​        ​nlp​.​add_pipe​(​ner​, ​last​=​True​) 
-  
- ​    ​# otherwise, get it, so we can add labels to it 
- ​    ​else​: 
- ​        ​ner​ ​=​ ​nlp​.​get_pipe​(​"ner"​) 
-  
- ​    ​# add labels 
- ​    ​for​ ​_​, ​annotations​ ​in​ ​TRAIN_DATA​: 
- ​        ​for​ ​ent​ ​in​ ​annotations​.​get​(​'entities'​): 
- ​            ​ner​.​add_label​(​ent​[​2​]) 
-  
- ​    ​# if model is None or reset_weights: 
- ​    ​#     optimizer = nlp.begin_training() 
- ​    ​# else: 
- ​    ​#     optimizer = nlp.resume_training() 
- ​    ​move_names​ ​=​ ​list​(​ner​.​move_names​) 
- ​    ​# get names of other pipes to disable them during training 
- ​    ​other_pipes​ ​=​ [​pipe​ ​for​ ​pipe​ ​in​ ​nlp​.​pipe_names​ ​if​ ​pipe​ ​!=​ ​"ner"​] 
- ​    ​with​ ​nlp​.​disable_pipes​(​*​other_pipes​):  ​# only train NER 
- ​        ​optimizer​ ​=​ ​nlp​.​begin_training​() 
- ​        ​# batch up the examples using spaCy's minibatch 
- ​        ​for​ ​itn​ ​in​ ​range​(​n_iter​): 
- ​            ​print​(​"Starting iteration "​ ​+​ ​str​(​itn​)) 
- ​            ​random​.​shuffle​(​TRAIN_DATA​) 
- ​            ​losses​ ​=​ {} 
- ​            ​for​ ​text​, ​annotations​ ​in​ ​TRAIN_DATA​: 
- ​                ​nlp​.​update​( 
- ​                    [​text​],  ​# batch of texts 
- ​                    [​annotations​],  ​# batch of annotations 
- ​                    ​drop​=​0.2​,  ​# dropout - make it harder to memorise data 
- ​                    ​sgd​=​optimizer​,  ​# callable to update weights 
- ​                    ​losses​=​losses​) 
- ​            ​print​(​"Losses"​, ​losses​) 
-  
- ​    ​# test the trained model 
- ​    ​test_text​ ​=​ ​"Marathwada Mitra Mandals College of Engineering" 
- ​    ​doc​ ​=​ ​nlp​(​test_text​) 
- ​    ​print​(​"Entities in '%s'"​ ​%​ ​test_text​) 
- ​    ​for​ ​ent​ ​in​ ​doc​.​ents​: 
- ​        ​print​(​ent​.​label_​, ​ent​.​text​) 
-  
- ​    ​# save model to output directory 
- ​    ​if​ ​output_dir​ ​is​ ​not​ ​None​: 
- ​        ​output_dir​ ​=​ ​Path​(​output_dir​) 
- ​        ​if​ ​not​ ​output_dir​.​exists​(): 
- ​            ​output_dir​.​mkdir​() 
- ​        ​nlp​.​meta​[​"name"​] ​=​ ​new_model_name​  ​# rename model 
- ​        ​nlp​.​to_disk​(​output_dir​) 
- ​        ​print​(​"Saved model to"​, ​output_dir​) 
-  
- ​        ​# test the saved model 
- ​        ​print​(​"Loading from"​, ​output_dir​) 
- ​        ​nlp2​ ​=​ ​spacy​.​load​(​output_dir​) 
- ​        ​# Check the classes have loaded back consistently 
- ​        ​assert​ ​nlp2​.​get_pipe​(​"ner"​).​move_names​ ​==​ ​move_names 
- ​        ​doc2​ ​=​ ​nlp2​(​test_text​) 
- ​        ​for​ ​ent​ ​in​ ​doc2​.​ents​: 
- ​            ​print​(​ent​.​label_​, ​ent​.​text​) 
-  
-  
- ​if​ ​__name__​ ​==​ ​"__main__"​: 
- ​    ​plac​.​call​(​main​)
